829011b49aa87876d19826cc11f5718c1f84c5ad
Kurt Cancemi <kurt@x64Architecture.com>
2014-03-01 14:18:34 -0500

    Added some tests

---
+++ Wnmp/Tests/TestOSInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Wnmp.Helpers;
+
+namespace Wnmp.Tests
+{
+    class TestOSInfo
+    {
+        [Test]
+        public void TestW81OSName()
+        {
+            int major = Environment.OSVersion.Version.Major;
+            int minor = Environment.OSVersion.Version.Minor;
+
+            if (major == 6 && minor == 3)
+            {
+                Assert.AreEqual("Windows 8.1", OSVersionInfo.Name);
+            }
+            else if (major == 6 && minor == 2)
+            {
+                Assert.AreEqual("Windows 8", OSVersionInfo.Name);
+            }
+            else if (major == 6 && minor == 1)
+            {
+                Assert.AreEqual("Windows 7", OSVersionInfo.Name);
+            }
+            else if (major == 6 && minor == 0)
+            {
+                Assert.AreEqual("Windows Vista", OSVersionInfo.Name);
+            }
+            else
+            {
+                Assert.Ignore();
+            }
+        }
+    }
+}
---
+++ Wnmp/Tests/TestOptions.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Wnmp.Configuration;
+
+namespace Wnmp.Tests
+{
+    class TestOptions
+    {
+        private Ini ini = new Ini();
+
+        [Test]
+        public void TestEditorSetting()
+        {
+            ini.UpdateSettings();
+            ini.editor = "C:/TestEditor";
+            ini.UpdateSettings();
+            ini.ReadSettings();
+
+            Assert.AreEqual("C:/TestEditor", ini.editor);
+        }
+        [Test]
+        public void TestStartUpWithWindowsSetting()
+        {
+            ini.UpdateSettings();
+            ini.startupwithwindows = true;
+            ini.UpdateSettings();
+            ini.ReadSettings();
+
+            Assert.AreEqual(true, ini.startupwithwindows);
+        }
+        [Test]
+        public void TestStartAllAppsAtLaunchSetting()
+        {
+            ini.UpdateSettings();
+            ini.startallapplicationsatlaunch = true;
+            ini.UpdateSettings();
+            ini.ReadSettings();
+
+            Assert.AreEqual(true, ini.startallapplicationsatlaunch);
+        }
+        [Test]
+        public void TestMinimizeWnmpToTraySetting()
+        {
+            ini.UpdateSettings();
+            ini.minimizewnmptotray = true;
+            ini.UpdateSettings();
+            ini.ReadSettings();
+
+            Assert.AreEqual(true, ini.minimizewnmptotray);
+        }
+        [Test]
+        public void TestAutoCheckForUpdatesSetting()
+        {
+            ini.UpdateSettings();
+            ini.autocheckforupdates = false;
+            ini.UpdateSettings();
+            ini.ReadSettings();
+
+            Assert.AreEqual(false, ini.autocheckforupdates);
+        }
+        [Test]
+        public void TestCheckForUpdateFrequencySetting()
+        {
+            ini.UpdateSettings();
+            ini.checkforupdatefrequency = 1;
+            ini.UpdateSettings();
+            ini.ReadSettings();
+
+            Assert.AreEqual(1, ini.checkforupdatefrequency);
+        }
+        [Test]
+        public void TestLastCheckForUpdateSetting()
+        {
+            ini.UpdateSettings();
+            ini.lastcheckforupdate = DateTime.Now;
+            ini.UpdateSettings();
+            ini.ReadSettings();
+
+            Assert.AreEqual(DateTime.Now.ToShortDateString(), ini.lastcheckforupdate.ToShortDateString());
+        }
+        [Test]
+        public void TestFirstRunSetting()
+        {
+            ini.UpdateSettings();
+            ini.firstrun = false;
+            ini.UpdateSettings();
+            ini.ReadSettings();
+
+            Assert.AreEqual(false, ini.firstrun);
+        }
+
+    }
+}
