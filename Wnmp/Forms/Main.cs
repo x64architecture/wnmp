489e722d0b7f8ff89f0f71b85408183a43f0f17b
Kurt Cancemi <kurt@x64Architecture.com>
2014-03-02 14:34:29 -0500

    Bump version.

--- Wnmp/Forms/Main.cs
+++ Wnmp/Forms/Main.cs
@@ -37,7 +37,7 @@ namespace Wnmp
     {
         public static string StartupPath { get { return Application.StartupPath; } }
 
-        private Version CPVER = new Version("2.2");
+        private Version CPVER = new Version("2.2.1");
         public Version GetCPVER { get { return CPVER; } }
 
         internal const string UpdateXMLURL = "https://s3.amazonaws.com/wnmp/update.xml";
