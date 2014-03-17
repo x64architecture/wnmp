x64architecture/wnmp

commit 9553809c8bf5010d411f66607462cfaa92e6108a

Removed ProcessStatus class as it was kinda pointless.

Wnmp/Programs/PHP.cs
@@ -41,7 +41,6 @@ namespace Wnmp.Programs
         public static ContextMenuStrip lms = new ContextMenuStrip(); // Log button context menu
         public static ToolTip PHP_start_Tip = new ToolTip(); // Start button ToolTip
         public static ToolTip PHP_stop_Tip = new ToolTip(); // Stop button ToolTip
-        public static int phpstatus = (int)ProcessStatus.ps.STOPPED; // Status
         internal static string pini = Application.StartupPath + "/php/php.ini"; // Location of php.ini to pass on to php
 
         /// <summary>
@@ -68,10 +67,7 @@ namespace Wnmp.Programs
         {
             /* The Win-PHP developers thought is was smart to kill php after a certain amount of requests (Probably 500). 
                so we have to restart it once it exits or set 'PHP_FCGI_MAX_REQUESTS' variable to 0. I've looked and people are recommending just to restart it. */
-            if (PHPStatus == (int)ProcessStatus.ps.STARTED) // Check if PHP is set to run
-            {
                 startprocess(@Application.StartupPath + "/php/php-cgi.exe", String.Format("-b localhost:9000 -c {0}", pini));
-            }
         }
 
         internal static void php_start_Click(object sender, EventArgs e)
@@ -82,7 +78,6 @@ namespace Wnmp.Programs
                 Log.wnmp_log_notice("Attempting to start PHP", Log.LogSection.WNMP_PHP);
                 Program.formInstance.phprunning.Text = "\u221A";
                 Program.formInstance.phprunning.ForeColor = Color.Green;
-                phpstatus = (int)ProcessStatus.ps.STARTED;
             }
             catch (Exception ex)
             {
@@ -99,7 +94,6 @@ namespace Wnmp.Programs
                 {
                     currentProc.Kill();
                 }
-                phpstatus = (int)ProcessStatus.ps.STOPPED;
             }
             catch (Exception ex)
             {
@@ -109,8 +103,6 @@ namespace Wnmp.Programs
             Program.formInstance.phprunning.Text = "X";
             Program.formInstance.phprunning.ForeColor = Color.DarkRed;
         }
-
-        public static int PHPStatus { get { return phpstatus; } }
 
         internal static void php_start_MouseHover(object sender, EventArgs e)
         {

Wnmp/Helpers/ProcessStatus.cs
@@ -1,127 +1,0 @@
-﻿/*
-Copyright (C) Kurt Cancemi
-
-This file is part of Wnmp.
-
-    Wnmp is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    Wnmp is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with Wnmp.  If not, see <http://www.gnu.org/licenses/>.
-*/
-using System;
-using System.Timers;
-using System.Diagnostics;
-using System.Security.Permissions;
-using Wnmp.Programs;
-using Wnmp.Helpers;
-
-namespace Wnmp.Helpers
-{
-    /// <summary>
-    /// Checks the status of a process to see if it matches the set ps value
-    /// </summary>
-    public class ProcessStatus
-    {
-        /// <summary>
-        /// Status of Process (ex. when the start button is clicked it is true)
-        /// </summary>
-        internal enum ps
-        {
-            STOPPED,
-            STARTED
-        }
-
-        public static void timer()
-        {
-            cfc = new Timer();
-            cfc.Elapsed += new ElapsedEventHandler(CheckProcessStatus);
-            cfc.Interval = 5000;
-            cfc.Enabled = true;
-        }
-
-        private static Timer cfc;
-        public delegate void Action();
-        /// <summary>
-        /// Checks the status of Nginx, MariaDB, and PHP and
-        /// restarts them if they crashed while they were already started.
-        /// </summary>
-        public static void CheckProcessStatus(Object source, ElapsedEventArgs e)
-        {
-            int ngxfails = 0;
-            int mariadbfails = 0;
-            int phpfails = 0;
-            switch (Nginx.NgxStatus)
-            {
-                case (int)ProcessStatus.ps.STARTED:
-                    {
-                        if (ngxfails <= 10) // If Nginx fails to start over 10 times quit trying to restart it.
-                        {
-                            if (ciair("nginx") == false)
-                            {
-                                Nginx.startprocess(Main.StartupPath + "/nginx.exe", "", false);
-                                Log.wnmp_log_error("Attempting to restart crashed Nginx", Log.LogSection.WNMP_NGINX);
-                                ngxfails++;
-                            }
-                        }
-                        break;
-                    }
-                case (int)ProcessStatus.ps.STOPPED: ngxfails = 0; break;
-            }
-            switch (MariaDB.MariaDBStatus)
-            {
-                case (int)ProcessStatus.ps.STARTED:
-                    {
-                        if (mariadbfails <= 10) // If MariaDB fails to start over 10 times quit trying to restart it.
-                        {
-                            if (ciair("mariadb") == false)
-                            {
-                                MariaDB.startprocess(Main.StartupPath + "/mariadb/bin/mysqld.exe", "", false, true, false);
-                                Log.wnmp_log_error("Attempting to restart crashed MariaDB", Log.LogSection.WNMP_MARIADB);
-                                mariadbfails++;
-                            }
-                        }
-                        break;
-                    }
-                case (int)ProcessStatus.ps.STOPPED: mariadbfails = 0; break;
-            }
-            switch (PHP.PHPStatus)
-            {
-                case (int)ProcessStatus.ps.STARTED:
-                    {
-                        if (phpfails <= 10) // If PHP fails to start over 10 times quit trying to restart it.
-                        {
-                            if (ciair("php-cgi") == false)
-                            {
-                                PHP.startprocess(Main.StartupPath + "/php/php-cgi.exe", "-b localhost:9000");
-                                Log.wnmp_log_error("Attempting to restart crashed PHP", Log.LogSection.WNMP_PHP);
-                                phpfails++;
-                            }
-                        }
-                        break;
-                    }
-                case (int)ProcessStatus.ps.STOPPED: phpfails = 0; break;
-            }
-        }
-        /// <summary>
-        /// Check if Process is running
-        /// </summary>
-        /// <returns>True if Process is running and false if not</returns>
-        private static bool ciair(string process)
-        {
-            Process[] ptcf = Process.GetProcessesByName(process);
-            if (ptcf.Length == 0)
-            {
-                return false;
-            }
-            else { return true; }
-        }
-    }
-}

Wnmp/Programs/MariaDB.cs
@@ -43,9 +43,6 @@ namespace Wnmp.Programs
         public static ToolTip MariaDB_start_Tip = new ToolTip(); // Start button ToolTip
         public static ToolTip MariaDB_stop_Tip = new ToolTip(); // Stop button ToolTip
         public static ToolTip MariaDB_opnshell_Tip = new ToolTip(); // Open Shell button ToolTip
-        public static int mariadbstatus = (int)ProcessStatus.ps.STOPPED; // Status
-
-        public static int MariaDBStatus { get { return mariadbstatus; } }
 
         /// <summary>
         /// Starts an executable file
@@ -75,7 +72,6 @@ namespace Wnmp.Programs
                 Log.wnmp_log_notice("Attempting to start MariaDB", Log.LogSection.WNMP_MARIADB);
                 Program.formInstance.mariadbrunning.Text = "\u221A";
                 Program.formInstance.mariadbrunning.ForeColor = Color.Green;
-                mariadbstatus = (int)ProcessStatus.ps.STARTED;
             }
             catch (Exception ex)
             {
@@ -99,21 +95,27 @@ namespace Wnmp.Programs
                 }
                 Program.formInstance.mariadbrunning.Text = "X";
                 Program.formInstance.mariadbrunning.ForeColor = Color.DarkRed;
-                mariadbstatus = (int)ProcessStatus.ps.STOPPED;
             }
             catch (Exception ex)
             {
                 Log.wnmp_log_error(ex.Message, Log.LogSection.WNMP_MARIADB);
             }
         }
-
+        private static bool MariaDBIsRunning()
+        {
+            Process[] ptcf = Process.GetProcessesByName("mysqld");
+            if (ptcf.Length == 0)
+                return false;
+            else
+                return true;
+        }
         internal static void mdb_shell_Click(object sender, EventArgs e)
         {
             try
             {
                 Log.wnmp_log_notice("Attempting to start MariaDB shell", Log.LogSection.WNMP_MARIADB);
                 // MariaDB
-                if (MariaDBStatus != 0)
+                if (!MariaDBIsRunning())
                 {
                     startprocess(@Application.StartupPath + @"\mariadb\bin\mysqld.exe", "", false, true, false);
                 }

Wnmp/Programs/Nginx.cs
@@ -43,9 +43,6 @@ namespace Wnmp.Programs
         private static ToolTip nginx_stop_Tip = new ToolTip(); // Stop button ToolTip
         private static ToolTip nginx_reload_Tip = new ToolTip(); // Reload button ToolTip
 
-        public static int ngxstatus = (int)ProcessStatus.ps.STOPPED; // Status
-        public static int NgxStatus { get { return ngxstatus; } }
-
         private static string NginxExe = Application.StartupPath.Replace(@"\", "/") + "/nginx.exe";
 
         /// <summary>
@@ -76,7 +73,6 @@ namespace Wnmp.Programs
                 Log.wnmp_log_notice("Attempting to start Nginx", Log.LogSection.WNMP_NGINX);
                 Program.formInstance.nginxrunning.Text = "\u221A";
                 Program.formInstance.nginxrunning.ForeColor = Color.Green;
-                ngxstatus = (int)ProcessStatus.ps.STARTED;
             }
             catch (Exception ex)
             {
@@ -99,7 +95,6 @@ namespace Wnmp.Programs
                 Log.wnmp_log_notice("Attempting to stop Nginx", Log.LogSection.WNMP_NGINX);
                 Program.formInstance.nginxrunning.Text = "X";
                 Program.formInstance.nginxrunning.ForeColor = Color.DarkRed;
-                ngxstatus = (int)ProcessStatus.ps.STOPPED;
             }
             catch (Exception ex)
             {

Wnmp/Wnmp.csproj
@@ -82,7 +82,6 @@
       <DependentUpon>ChangelogViewer.cs</DependentUpon>
     </Compile>
     <Compile Include="Programs\General.cs" />
-    <Compile Include="Helpers\ProcessStatus.cs" />
     <Compile Include="Helpers\Updater.cs" />
     <Compile Include="Forms\About.cs">
       <SubType>Form</SubType>

commit 32cb04ac8ccb6f0d7683c5c800372c012d8751c0

Fixed PHP not exiting caused by 9553809c8b
Signed-off-by: Kurt Cancemi <kurt@x64Architecture.com>

Wnmp/Programs/PHP.cs
@@ -45,6 +45,14 @@ namespace Wnmp.Programs
         public static ToolTip PHP_stop_Tip = new ToolTip(); // Stop button ToolTip
         internal static string pini = Application.StartupPath + "/php/php.ini"; // Location of php.ini to pass on to php
 
+        private enum Status
+        {
+            Stopped = 0,
+            Started = 1
+        }
+
+        private static Status PHPStatus = Status.Stopped;
+
         /// <summary>
         /// Starts an executable file
         /// </summary>
@@ -60,8 +68,11 @@ namespace Wnmp.Programs
             ps.StartInfo.CreateNoWindow = true; // Excute with no window
             ps.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             ps.StartInfo.EnvironmentVariables.Add("PHP_FCGI_MAX_REQUESTS", "2000"); // After 2000 requests php-cgi.exe will kill its process
-            ps.EnableRaisingEvents = true;
-            ps.Exited += new EventHandler(ps_Exited); // Were going to have to restart PHP after its process is killed.
+            if (PHPStatus != Status.Stopped)
+            {
+                ps.EnableRaisingEvents = true;
+                ps.Exited += new EventHandler(ps_Exited); // Were going to have to restart PHP after its process is killed.
+            }
             ps.Start(); // Start the process
         }
 
@@ -69,7 +80,10 @@ namespace Wnmp.Programs
         {
             /* The Win-PHP developers thought is was smart to kill php after a certain amount of requests (Probably 500). 
                so we have to restart it once it exits or set 'PHP_FCGI_MAX_REQUESTS' variable to 0. I've looked and people are recommending just to restart it. */
+            if (PHPStatus != Status.Stopped)
+            {
                 startprocess(@Application.StartupPath + "/php/php-cgi.exe", String.Format("-b localhost:9000 -c {0}", pini));
+            }
         }
 
         internal static void php_start_Click(object sender, EventArgs e)
@@ -77,6 +91,7 @@ namespace Wnmp.Programs
             try
             {
                 startprocess(@Application.StartupPath + "/php/php-cgi.exe", String.Format("-b localhost:9000 -c {0}", pini));
+                PHPStatus = Status.Started;
                 Log.wnmp_log_notice("Attempting to start PHP", Log.LogSection.WNMP_PHP);
                 Declarations.ToStartedLabel(Program.formInstance.phprunning);
             }
@@ -95,6 +110,7 @@ namespace Wnmp.Programs
                 {
                     currentProc.Kill();
                 }
+                PHPStatus = Status.Stopped;
             }
             catch (Exception ex)
             {