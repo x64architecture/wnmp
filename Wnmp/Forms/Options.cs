385e6e348bc1d1b14548304ed8a3e0e67e229f4c
Kurt Cancemi <kurt@x64Architecture.com>
2014-03-26 23:07:40 -0400

    Fixed compile error caused by invalid comment

--- Wnmp/Forms/Options.cs
+++ Wnmp/Forms/Options.cs
@@ -15,7 +15,6 @@ This file is part of Wnmp.
 
     You should have received a copy of the GNU General Public License
     along with Wnmp.  If not, see <http://www.gnu.org/licenses/>.
-*/along with Wnmp.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 using System;
