a03c40958a0d25da3f01baa9d63ba8313bcd11ca
Kurt Cancemi <kurt@x64Architecture.com>
2014-06-17 22:00:22 -0400

    Improved Updater code
    Basically I removed a lot of nonsense.

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -20,13 +20,14 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 
 
 namespace Updater
 {
     class Program
     {
-        
+
         /// <summary>
         /// Trys to find the process in the name parameter
         /// and if it does it kills it
@@ -34,7 +35,7 @@ namespace Updater
         /// <returns>True on sucess else false</returns>
         public static bool FindAndKillProcess(string name)
         {
-            foreach (var process in Process.GetProcesses())
+            foreach (Process process in Process.GetProcesses())
             {
                 if (process.ProcessName.Contains(name))
                 {
@@ -58,61 +59,30 @@ namespace Updater
         /// </summary>
         static void Main(string[] args)
         {
-        var print_log = false;
-        var curDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        var _orig = Path.Combine(curDir, "Wnmp.exe");
-        var _new = Path.Combine(curDir, "Wnmp_new.exe");
-        var _old = Path.Combine(curDir, "Wnmp_old.exe");
-            foreach (var s in args)
-            {
-                if (s == "-t")
-                    print_log = true;
-            }
-            if (print_log)
-            {
-                Console.WriteLine(true);
-            }
+            string curDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string _orig = Path.Combine(curDir, "Wnmp.exe");
+            string _new = Path.Combine(curDir, "Wnmp_new.exe");
+
             if (!FindAndKillProcess("Wnmp"))
             {
                 Console.WriteLine("Failed To Kill the process");
                 return;
             }
-            try
-            {
-                System.Threading.Thread.Sleep(500);
-                if (File.Exists(_orig))
-                {
-                    File.Move(_orig, _old);
-                    if (print_log)
-                    {
-                        Console.WriteLine("Moved {0} to {1}", _orig, _old);
-                    }
-                }
-                if (File.Exists(_new))
-                {
-                    File.Move(_new, _orig);
 
-                    if (print_log)
-                    {
-                        Console.WriteLine("Moved {0} to {1}", _new, _orig);
-                    }
-                    File.Delete(_old);
-                    if (print_log)
-                    {
-                        Console.WriteLine("Deleted {0}", _old);
-                    }
-                    Process.Start(_orig);
-                    if (print_log)
-                        Console.WriteLine("Started Wnmp.exe and exited the updater.");
-                        
-                    Process.GetCurrentProcess().Kill();
-                }
-            }
-            catch (Exception ex)
-            {
-                File.WriteAllText("updaterlog.txt", ex.ToString());
-                Console.WriteLine(ex.Message);
-            }
+            Thread.Sleep(250);
+
+            if (!File.Exists(_orig))
+                return;
+            
+            File.Delete(_orig);
+
+            if (!File.Exists(_new))
+                return;
+            
+            File.Move(_new, _orig);
+
+            Process.Start(_orig);
+            return;
         }
     }
 }
