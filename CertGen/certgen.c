ff9e25aa5ebbaede0e0e20f347e29cd49e275234
Kurt Cancemi <kurt@x64Architecture.com>
2014-07-15 15:41:15 -0400

    Update license

--- CertGen/certgen.c
+++ CertGen/certgen.c
@@ -1,7 +1,19 @@
 /*
-* Copyright (c) Kurt Cancemi 2014
-* Licensed under the OpenSSL license http://www.openssl.org/source/license.html
-*/
+ * Copyright (c) 2014, Kurt Cancemi (kurt@x64architecture.com)
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #ifndef _MSVC_
 #define _CRT_SECURE_NO_WARNINGS
 #endif
