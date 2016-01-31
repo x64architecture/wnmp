6d0d4d6b84aba1a057f4b119783b72a1967e82e5
Kurt Cancemi <kurt@x64architecture.com>
2016-01-30 23:18:53 -0500

    Fix editor option.

--- Wnmp/Configuration/Options.cs
+++ Wnmp/Configuration/Options.cs
@@ -80,7 +80,7 @@ namespace Wnmp.Forms
 
         private void SetSettings()
         {
-            settings.Editor = Editor;
+            settings.Editor = EditorTB.Text;
             settings.StartWithWindows = StartWnmpWithWindows.Checked;
             settings.RunAppsAtLaunch = StartAllProgramsOnLaunch.Checked;
             settings.MinimizeWnmpToTray = MinimizeWnmpToTray.Checked;
