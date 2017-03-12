b6ae27ba0e6d7d2d191f590c306f37e3629a461c
Kurt Cancemi <kurt@x64architecture.com>
2017-03-12 01:28:04 -0500

    Misc

--- Wnmp/html/index.php
+++ Wnmp/html/index.php
@@ -3,7 +3,7 @@
 <title>Welcome to Wnmp!</title>
 </head>
 <body>
-<center><h2>Welcome to Wnmp 2.2.4!</h2></center>
+<center><h2>Welcome to Wnmp 3.0.0!</h2></center>
 <center><h2>by <a href="https://www.x64architecture.com/">Kurt Cancemi</a></h2></center>
 <center><h2><a href="https://www.getwnmp.org/">Wnmp Website</a></h2></center>
 <center>Click <a href="http://localhost/phpmyadmin/">here</a> to go to phpMyAdmin</center>
